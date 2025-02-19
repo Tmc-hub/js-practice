5e04075de2e65dd6747e061142fb061af7495729
Tmc-hub <tmch9404@gmail.com>
2025-02-18 21:02:46 -0500

    day-1 added

---
+++ day-1/helloWorld.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+const goodbye = "Goodbye, World!";
+console.log("TaeMin");
+const lastName = "Cho";
+console.log("TaeMin", lastName);
+console.log(123);
+console.log("Hello", "I'm", "Trying");
