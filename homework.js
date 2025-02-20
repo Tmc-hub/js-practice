213ad557d0d0d9dc92916c2f6e2f4f0cdca5f4fb
Tmc-hub <tmch9404@gmail.com>
2025-02-20 18:23:36 -0500

    homework day one

---
+++ homework.js
@@ -0,0 +1,13 @@
+console.log("Good morning, everyone!");
+const myName = "TaeMin Cho";
+console.log("Pizza, Ice Cream, burger");
+const currentYear = 2025;
+const nextYear = 2026;
+console.log(3.14);
+const age = 31;
+console.log(
+  "My name is " + myName + " " + "and I am " + age + " " + "years old"
+);
+console.log(
+  "The current year is " + currentYear + " " + "next year will be " + nextYear
+);
