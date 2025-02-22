f5f73dce7b302e32a3253d1688b93bb460564e43
Tmc-hub <tmch9404@gmail.com>
2025-02-22 01:52:44 -0500

    homework2 added

--- day-1/helloWorld.js
+++ day-1/helloWorld.js
@@ -5,3 +5,79 @@ const lastName = "Cho";
 console.log("TaeMin", lastName);
 console.log(123);
 console.log("Hello", "I'm", "Trying");
+
+const myString = "This is a string";
+const myNumber = 123;
+const myBoolean = true;
+const myUndefined = undefined;
+const myNull = null;
+const myArray = [1, 2, 3, 4, 5];
+const myObject = {
+  firstName: "Tae",
+  lastName: "Cho",
+};
+//Variables are block scopes
+
+//const = constant
+//Cannot be changed
+
+//Var and let can be redefined
+
+const stringVariable = "Chicken";
+const numVariable = 9000;
+const objectVariable = {
+  personName: "TaeMin",
+  Height: 6 + "ft",
+};
+const hobbies = ["Eating", "Watching Movies", "Playing Games"];
+const movies = ["LotR", "Star Wars"];
+console.log(objectVariable);
+console.log(hobbies);
+
+let cars = "mercedes";
+console.log(cars);
+
+const num1 = 4;
+const num2 = 5;
+const num3 = 6;
+const num4 = 7;
+const num5 = 8;
+const num6 = 10;
+console.log(num1 + num2);
+console.log(num3 + num4);
+console.log(num4 - num1);
+console.log(num3 - num2);
+console.log(num1 * num2);
+console.log(num3 * num6);
+console.log(num5 / num1);
+console.log(num6 / num2);
+console.log(num6 % num1);
+console.log(num5 % num2);
+
+console.log(movies[1]);
+
+const arr = ["bob", "pat", "mike", "sean"];
+// print "sean"
+console.log(arr[3]);
+const obj = {
+  make: "honda",
+  model: "civic",
+  year: 2002,
+  mileage: 100000,
+};
+
+// print the mileage
+console.log(obj.mileage);
+
+const arrayOfObjects = [
+  { name: "phil" },
+  { name: "marco" },
+  {
+    name: "kaiser",
+  },
+  {
+    name: "miran",
+  },
+];
+// print 'kaiser'
+console.log(arrayOfObjects[2]);
---
+++ day-1/homework2.js
@@ -0,0 +1,45 @@
+const myName = "TaeMin";
+const age = 30;
+const weatherSunny = true;
+const food = ["Pizza", "Ice Cream", "Burgers"];
+const names = {
+  firstName: "TaeMin",
+  lastName: "Cho",
+};
+console.log(10 + 5);
+console.log(6 * 4);
+console.log(20 - 7);
+console.log(50 / 5);
+console.log(10 % 3);
+console.log("Good " + "Morning!");
+const arrayone = ["Miran", "Kaiser", "Tae", "Monica"];
+console.log(arrayone[0]);
+console.log(arrayone[3]);
+console.log(arrayone[1]);
+console.log(arrayone[2]);
+const person = { firstName: "Tae", lastName: "Cho", age: 30, isStudent: true };
+console.log(person.firstName);
+console.log(person["age"]);
+console.log(person["lastName"]);
+console.log(
+  "Hello, my name is " +
+    names.firstName +
+    " " +
+    names.lastName +
+    "and I am " +
+    age +
+    " years old"
+);
+const num1 = 5;
+const num2 = 3;
+const result = num1 + num2;
+console.log("The result of " + num1 + " and " + num2 + " is " + result);
+
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "1984", author: "George Orwell", year: 1949 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+];
+console.log(books[0].title);
+console.log(books[2].author);
+console.log(books[1].year);
